2b0a73adaaef91d810254783ace2e9cb510126a6
safeBelt <cbh0000000@gmail.com>
2019-07-06 23:29:54 +0900

    React Text File upload & Move to Detail

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import AppShell from './AppShell';
 import Home from './Home';
 import Texts from './Texts';
 import Words from './Words';
+import Detail from './Detail';
 
 
 class App extends React.Component{
@@ -16,7 +17,7 @@ class App extends React.Component{
                             <Route exact path="/" component={Home}/>
                             <Route exact path="/Texts" component={Texts}/>
                             <Route exact path="/Words" component={Words}/>
-
+                            <Route exact path="/detail/:textID" component={Detail}/>
                         </div>
                     </AppShell>
             </Router>
