c9aa9d3145fcce02d9c2ab3ccda38f4b20e17371
safebelt <cbh0000000@gmail.com>
2019-07-06 12:02:29 +0900

    Add Firebase Database & Word [View, Add, Delete]

--- src/components/AppShell.js
+++ src/components/AppShell.js
@@ -45,17 +45,17 @@ class AppShell extends React.Component {
                     <Drawer open={this.state.toggle}>
                         <MenuItem onClick={this.handleDrawerToggle}>
                             <Link component={RouterLink} to="/">
-                                Home
+                                홈 화면
                                 </Link>
                         </MenuItem>
                         <MenuItem onClick={this.handleDrawerToggle}>
                             <Link component={RouterLink} to="/Texts">
-                                Texts
+                                텍스트 관리
                                 </Link>
                         </MenuItem>
                         <MenuItem onClick={this.handleDrawerToggle}>
                             <Link component={RouterLink} to="/Words">
-                                words
+                                단어 관리
                                 </Link>
                         </MenuItem>
                         </Drawer>
